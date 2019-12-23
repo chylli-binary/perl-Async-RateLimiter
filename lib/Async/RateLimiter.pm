f0950e1292cb24bc65514884a03efb7ab2dbe7f6
chylli-binary <chylli@binary.com>
2019-12-23 16:26:17 +0800

    xtest pass

--- lib/Async/RateLimiter.pm
+++ lib/Async/RateLimiter.pm
@@ -1,5 +1,4 @@
 package Async::RateLimiter;
-# ABSTRACT: ...
 
 use strict;
 use warnings;
@@ -23,15 +22,112 @@ Async::RateLimiter - Module abstract
 
 =cut
 
+use Carp qw();
+use Future;
+use mro;
+
+use parent qw(IO::Async::Notifier);
+
+=head1 NAME
+
+WebService::Async::CustomerIO::RateLimitter - This class provide possibility to limit amount
+of request in time interval
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=cut
+
+sub _init {
+    my ($self, $args) = @_;
+    for my $k (qw(limit interval)) {
+        die "Missing required argument: $k" unless exists $args->{$k};
+        die "Invalid value for $k: $args->{$k}" unless int($args->{$k}) > 0;
+        $self->{$k} = delete $args->{$k} if exists $args->{$k};
+    }
+
+    $self->{queue}   = [];
+    $self->{counter} = 0;
+
+    return $self->next::method($args);
+}
+
+=head2 interval
+
+=cut
+
+sub interval { return shift->{interval} }
+
+=head2 limit
+
+=cut
+
+sub limit { return shift->{limit} }
+
+=head2 acquire
+
+Method checks availability for free slot.
+It returns future, when slot will be available, then future will be resolved.
+
+=cut
+
+sub acquire {
+    my ($self) = @_;
+
+    $self->_start_timer;
+    return Future->done if ++$self->{counter} <= $self->limit;
+
+    my $current = $self->_current_queue;
+    $current->{counter}++;
+    return $current->{future};
+}
+
+sub _current_queue {
+    my ($self) = @_;
+
+    # +1 for getting correct position for edge cases like: limit 2, counter 4, should be 0
+    my $pos = int(($self->{counter} - ($self->limit + 1)) / $self->limit);
+
+    $self->{queue}[$pos] //= {
+        future  => $self->loop->new_future,
+        counter => 0
+    };
+
+    return $self->{queue}[$pos];
+}
+
+sub _start_timer {
+    my ($self) = @_;
+
+    $self->{timer} //= $self->loop->delay_future(
+        after => $self->interval,
+        )->on_ready(
+        sub {
+            $self->{counter} = 0;
+            delete $self->{timer};
+
+            return unless @{$self->{queue}};
+
+            $self->_start_timer;
+
+            my $current = shift @{$self->{queue}};
+            $self->{counter} = $current->{counter};
+            $current->{future}->done;
+        });
+
+    return $self->{timer};
+}
+
 1;
 
 =head1 AUTHOR
 
-Chylli <chylli@163.com>
+Binary.com
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2019 by Chylli.
+This software is copyright (c) 2019 by Binary.com.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
