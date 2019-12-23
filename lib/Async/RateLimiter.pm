08433b7d035c30aa80a3cfbe67eedfb0216c062c
chylli-binary <chylli@binary.com>
2019-12-23 16:10:09 +0800

    initial commit

---
+++ lib/Async/RateLimiter.pm
@@ -0,0 +1,47 @@
+package Async::RateLimiter;
+# ABSTRACT: ...
+
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+=head1 NAME
+
+Async::RateLimiter - Module abstract
+
+=head1 SYNOPSIS
+
+    use Async::RateLimiter;
+    my $instance = Async::RateLimiter->new;
+
+=head1 DESCRIPTION
+
+=cut
+
+=head1 METHODS
+
+=cut
+
+1;
+
+=head1 AUTHOR
+
+Chylli <chylli@163.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2019 by Chylli.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+
+=head1 SEE ALSO
+
+=over 4
+
+=item *
+
+=back
+
